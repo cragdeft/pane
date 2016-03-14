0dc843275c9db0367818ce0c635d4f3d9b3b24dc
sumon.kumar <sumon.kumar@aplombtechbd.com>
2016-03-14 12:50:03 +0600

    add display property

--- AplombTech.DWasa.Entity/AddressEntity.cs
+++ AplombTech.DWasa.Entity/AddressEntity.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,9 @@ namespace AplombTech.DWasa.Entity
 {
     public class AddressEntity
     {
+        [Display(Name = "Street 1")]
         public string Street1 { get;  set; }
+        [Display(Name = "Street 2")]
         public string Street2 { get;  set; }
         public string Zip { get;  set; }
         public string City { get;  set; }
