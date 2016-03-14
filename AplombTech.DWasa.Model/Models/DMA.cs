699e343a89321ef65c14b2204bbf3a113b28b099
sumon.kumar <sumon.kumar@aplombtechbd.com>
2016-03-14 11:34:06 +0600

    add entity

---
+++ AplombTech.DWasa.Model/Models/DMA.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AplombTech.DWasa.Model.Models
+{
+    public class DMA : Area
+    {
+        #region Primitive Properties
+       
+        #endregion
+
+        #region  Complex Properties
+        #endregion
+
+        #region  Navigation Properties
+
+        public virtual Zone Zone { get; set; }
+        #endregion
+        
+    }
+}
