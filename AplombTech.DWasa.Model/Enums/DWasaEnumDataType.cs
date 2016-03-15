1526688f15886cca7f9f6d5fa74850531661985b
sumon.kumar <sumon.kumar@aplombtechbd.com>
2016-03-15 13:01:18 +0600

    remove enum from model

--- AplombTech.DWasa.Model/Enums/DWasaEnumDataType.cs
+++ AplombTech.DWasa.Model/Enums/DWasaEnumDataType.cs
@@ -1,15 +1,24 @@
 ﻿namespace AplombTech.DWasa.Model.Enums
 {
-    public enum CommandType
-    {
-        Command,
-        Configuration,
-        Feedback
-    }
+    //public enum CommandType
+    //{
+    //    Command,
+    //    Configuration,
+    //    Feedback
+    //}
 
-    public enum NetworkType
-    {
-        Gsm,
-        BroadBand
-    }
+    //public enum NetworkType
+    //{
+    //    Gsm,
+    //    BroadBand
+    //}
+
+    //public enum SensorType
+    //{
+    //    WaterLevel,
+    //    Cholorination,
+    //    Energy,
+    //    Production,
+    //    Pressure
+    //}
 }
