b34c5182a2b73f134ebe58db3c36d00d9c70395b
sumon.kumar <sumon.kumar@aplombtechbd.com>
2016-03-15 13:05:13 +0600

    add new project

---
+++ AplombTech.DWasa.Utility/Enums/DWasaEnumDataType.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AplombTech.DWasa.Utility.Enums
+{
+    public enum CommandType
+    {
+        Command,
+        Configuration,
+        Feedback
+    }
+
+    public enum NetworkType
+    {
+        Gsm,
+        BroadBand
+    }
+
+    public enum SensorType
+    {
+        WaterLevel,
+        Cholorination,
+        Energy,
+        Production,
+        Pressure
+    }
+}
