9cae0faf9e89d2d73da9065c0d9d12da38f3d2fa
sumon.kumar <sumon.kumar@aplombtechbd.com>
2016-04-07 08:51:03 +0600

    commit sensor data add

--- WMS_VS2015_NO/AplombTech.WMS/Console Application/AplombTech.WMS.MQTT.Client/NakedObjectsRunSettings.cs
+++ WMS_VS2015_NO/AplombTech.WMS/Console Application/AplombTech.WMS.MQTT.Client/NakedObjectsRunSettings.cs
@@ -13,6 +13,7 @@ using NakedObjects.Menu;
 using NakedObjects.Core.Async;
 using AplombTech.WMS.Domain.Repositories;
 using AplombTech.WMS.Domain.Facade;
+using AplombTech.WMS.Domain.Sensors;
 
 namespace AplombTech.WMS.MQTT.Client {
 
@@ -42,7 +43,7 @@ namespace AplombTech.WMS.MQTT.Client {
 		private static Type[] Types {
             get {
                 return new Type[] {
-                    //typeof(MqttClientWrapper)
+                    typeof(SensorData)
                 };
             }
         }
