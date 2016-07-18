8725ac3fdcb4c9470219af6e08e0911a87bcf3f9
sumon.kumar <sumon.kumar@aplombtechbd.com>
2016-07-18 12:12:31 +0600

    add datalog in areacontext

--- WMS_VS2015_NO/AplombTech.WMS/Infrastructure/AplombTech.WMS.AreaBoundedContext/AreaContext.cs
+++ WMS_VS2015_NO/AplombTech.WMS/Infrastructure/AplombTech.WMS.AreaBoundedContext/AreaContext.cs
@@ -36,7 +36,7 @@ namespace AplombTech.WMS.AreaBoundedContext
         public DbSet<PressureSensor> PressureSensors { get; set; }
         public DbSet<Address> Addresses { get; set; }
         public DbSet<Unit> Units { get; set; }
-
+        public DbSet<DataLog> SensorDataLogs { get; set; }
         public DbSet<SensorData> SensorDatas { get; set; }
         public DbSet<MotorData> MotorDatas { get; set; }
         public DbSet<SensorHourlySummaryData> SensorHourlySummaryData { get; set; }
