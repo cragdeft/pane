a636a92feab701a429f7fe6601dcbca5e23329df
admin <Mh01369>
2016-09-06 11:21:12 +0600

    HttpWebRequest has been replaced with HttpClient in Email Sending method

--- WMS_VS2015_NO/AplombTech.WMS/Infrastructure/AplombTech.WMS.Utility/EmailSender.cs
+++ WMS_VS2015_NO/AplombTech.WMS/Infrastructure/AplombTech.WMS.Utility/EmailSender.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,19 +11,28 @@ namespace AplombTech.WMS.Utility
 {
     public class EmailSender
     {
+        private static readonly HttpClient HttpClient = new HttpClient();
         public static string SendEmail(string to, string from, string subject, string body)
         {
             try
             {
                 string smtpAddress = "wasa-noreply@sinepulse.net ";
                 string smtpPassword = "hX3uJtj9";
-                var httpReq = (HttpWebRequest)WebRequest.Create("http://emailservice.azurewebsites.net/EmailService.svc/SendMail?to=" + to + "&from=" + from + "&subject=" + subject + "&body=" + body + "&smtpAddress=" + smtpAddress + "&smtpPassword=" + smtpPassword);
-                httpReq.Method = "POST";
-                httpReq.ContentType = "application/x-www-form-urlencoded";
-                httpReq.ContentLength = 0;
 
-                var response = (HttpWebResponse)httpReq.GetResponse();
-                string responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+                string uri = "http://emailservice.azurewebsites.net/EmailService.svc/SendMail?to=" + to + "&from=" +
+                             from + "&subject=" + subject + "&body=" + body + "&smtpAddress=" + smtpAddress +
+                             "&smtpPassword=" + smtpPassword;
+
+                //var httpReq = (HttpWebRequest)WebRequest.Create(uri);
+                //httpReq.Method = "POST";
+                //httpReq.ContentType = "application/x-www-form-urlencoded";
+                //httpReq.ContentLength = 0;
+                //var response = (HttpWebResponse)httpReq.GetResponse();
+                //string responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+
+                var result = HttpClient.GetAsync(uri).Result;
+                result.EnsureSuccessStatusCode();
+                string responseString = result.Content.ReadAsStringAsync().Result;
 
                 if (responseString.Contains("Success"))
                 {
